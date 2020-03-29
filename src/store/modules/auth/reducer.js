05cc30746ef369735e28cdb2f860194960a2ecea
Lucas Iori <lucasferiori@gmail.com>
2020-03-29 17:48:02 -0300

    Criadas funcionalidades de perfil do usuário

--- src/store/modules/auth/reducer.js
+++ src/store/modules/auth/reducer.js
@@ -23,6 +23,11 @@ export default function auth(state = INITIAL_STATE, action) {
         draft.loading = false;
         break;
       }
+      case '@auth/SIGN_out': {
+        draft.token = null;
+        draft.signed = false;
+        break;
+      }
       default:
     }
   });
